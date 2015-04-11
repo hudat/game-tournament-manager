f35fae61f3fc7122e75b66c1727793dbb55c0c23
Husam Machlovi <hmachlovi@gmail.com>
2015-04-11 18:32:27 -0400

    insert players and remove

--- foosboom-meteor/client/views/teams.js
+++ foosboom-meteor/client/views/teams.js
@@ -12,13 +12,28 @@ Template.teams.events({
     e.preventDefault();
     Session.set('isCreatingTeam', true);
   },
+
   'click a.cancel': function(e, tpl){
     e.preventDefault();
     Session.set('isCreatingTeam', false);
   },
+
   'submit form.create-team': function(e, tpl){
     e.preventDefault();
     var teamName = tpl.$('input[name=name]').val();
-    Teams.insert({name: teamName});
+    Teams.insert({name: teamName}, function(error, _id){
+      if(error){
+        alert(error);
+        Session.set('isCreatingTeam', true);
+        Tracker.afterFlush(function(){
+          tpl.$('input[name=name]').val(teamName);
+        });
+      }
+    });
+  },
+
+  'click a.remove': function(e, tpl){
+    e.preventDefault();
+    Teams.remove(this._id);
   }
 });
