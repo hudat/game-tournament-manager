fc3d49a3d4744b8d7946d33944207a4bd7ef1cab
Husam Machlovi <hmachlovi@gmail.com>
2015-04-12 18:27:51 -0400

    ability to edit team name by clicking on name

--- foosboom-meteor/client/views/team.js
+++ foosboom-meteor/client/views/team.js
@@ -5,8 +5,28 @@ Template.team.helpers({
 });
 
 Template.team.events({
+  "click a.edit": function(e, tpl){
+    e.preventDefault();
+    Session.set('isEditingTeam', this._id);
+  },
+
+  "submit form.form-edit": function(e,tpl){
+    e.preventDefault();
+
+    var teamName = tpl.$("input[name='name']").val();
+    if(teamName.length){
+      Teams.update(this._id, {$set: {name: teamName}});
+      Session.set('isEditingTeam', null);
+    }
+  },
+
+  "click a.cancel": function(e, tpl){
+    e.preventDefault();
+    Session.set('isEditingTeam', null);
+  },
+
   'click a.remove': function(e, tpl){
     e.preventDefault();
     Teams.remove(this._id);
   }
-})
+});
