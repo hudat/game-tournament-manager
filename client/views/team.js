84b52cb02d3a4bbe25d143a4a368702fc0f7080f
Husam Machlovi <hmachlovi@gmail.com>
2015-04-14 18:32:08 -0400

    method to maintain team name integrity

--- client/views/team.js
+++ client/views/team.js
@@ -28,5 +28,34 @@ Template.team.events({
   'click a.remove': function(e, tpl){
     e.preventDefault();
     Teams.remove(this._id);
-  }
+  },
+
+  "submit form.form-edit": function(e, tpl){
+      e.preventDefault();
+
+      var teamName = tpl.$("input[name='name']").val();
+      var self = this;
+
+      if(teamName.length){
+        Teams.update(this._id, {$set: {name: teamName}}, function(error){
+
+          if(!error){
+
+            // Update games this team is a part of
+            var games = Games.find({_id: {$in: self.gameIds}});
+            if(games.count()){
+              _(games.fetch()).each(function(game){
+                var team = _(game.teams).findWhere({id: self._id});
+                if(team != null){
+                  team.name = teamName;
+                  Games.update({_id: game._id}, {$set: {teams: game.teams}})
+                }
+              });
+            }
+          }
+        });
+
+        Session.set('editedTeamId', null);
+      }
+    }
 });
