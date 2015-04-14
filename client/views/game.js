797485021172c48d876ddc1afd5204d9e2a96ab7
Husam Machlovi <hmachlovi@gmail.com>
2015-04-14 18:09:16 -0400

    game deletion

--- client/views/game.js
+++ client/views/game.js
@@ -27,5 +27,19 @@ Template.game.events({
   "click a.two-minus": function(e, tpl){
     e.preventDefault();
     Games.update({_id: this._id}, {$inc: {"teams.1.score": -1}});
+  },
+
+  "click a.delete-game": function(e, tpl){
+    e.preventDefault();
+    var gameId = this._id;
+    var teamIdA = this.teams[0].id;
+    var teamIdB = this.teams[1].id;
+
+    Games.remove(gameId, function (error) {
+      if (!error) {
+        Teams.update({_id: teamIdA}, {$pull: {gameIds: gameId}});
+        Teams.update({_id: teamIdB}, {$pull: {gameIds: gameId}});
+      }
+    });
   }
 });
