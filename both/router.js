65684e870f4daf37d15f671c7da94cbea72766c3
Husam Machlovi <hmachlovi@gmail.com>
2015-04-25 01:09:28 -0400

    added subscriptions to routes

--- both/router.js
+++ both/router.js
@@ -2,5 +2,15 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
-Router.route('/', {name: 'games'});
-Router.route('/teams');
+Router.route('/', {
+  name: 'games',
+  waitOn: function(){
+    return [Meteor.subscribe("games"), Meteor.subscribe("teams")];
+  }
+});
+
+Router.route('/teams', {
+  waitOn: function(){
+    return Meteor.subscribe("teams");
+  }
+});
